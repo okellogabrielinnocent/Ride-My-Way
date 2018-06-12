127ffb79897f39426d4becb5a56b76d36dbfb52e
Gabriel Innocent <okellogabrielinnocent@gmail.com>
2018-06-12 09:48:15 +0300

    Registration form was created and I updated the CSS, and Log in form in index.html

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
+const header = document.getElementById("myHeader");
+const sticky = header.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
@@ -10,3 +10,8 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+$(document).ready(function(){
+  $('#login1').tooltip('show');
+  $('#login1').tooltip('hide');
+});
